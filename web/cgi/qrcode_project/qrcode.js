bbe04069239a82ea7ade52e30ee1f52ae1982f1b
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2024-03-02 18:18:44 -0500

    Add background picture to QR code.

--- web/cgi/qrcode_project/qrcode.js
+++ web/cgi/qrcode_project/qrcode.js
@@ -1,11 +1,36 @@
 window.addEventListener("load", ()=>{
-	form = document.querySelector("#mainForm");
-	img = document.querySelector("#qrimg");
-	form.addEventListener("submit", (e)=>{
+	const form = document.querySelector("#mainForm");
+	const canvas = document.getElementById("can");
+	form.addEventListener("submit", async (e)=>{
 		if (e.preventDefault) e.preventDefault();
-		msg = document.getElementById("msg").value;	
-		img.src = `${form.action}?msg=${msg}`
-		img.style.display = "inline";
+		const qr = await fetchQR(form.action);
+		const img = document.createElement("img");
+		img.onload = ()=>{
+			canvas.width = img.width;
+			canvas.height = img.height;
+			const ctx = canvas.getContext('2d');
+			ctx.drawImage(img, 0, 0);
+		};
+		img.src = URL.createObjectURL(qr);
 		return false;
 	});
 });
+
+async function fetchQR(url){
+
+	const formData = new FormData();
+	const bgFileE = document.getElementById("bg");
+
+	const msg = document.getElementById("msg").value;	
+	formData.append("msg", msg);
+	formData.append("bg", bgFileE.files[0]);
+
+	const response = await fetch(url, {
+		method: "POST",
+		mode: "cors",
+		body: formData,
+	});
+
+	const qr = await response.blob();
+	return qr;
+}
--- web/cgi/qrcode_project/qrcode.py
+++ web/cgi/qrcode_project/qrcode.py
@@ -10,15 +10,34 @@ def cgi_main():
 
     form = cgi.FieldStorage()
     msg = form.getfirst("msg", "unknown")
-    qrcode = segno.make(msg)
-    qrcode.save(sys.stdout.buffer, kind="png", scale=10)
+
+    bg_file = None
+    if 'bg' in form:
+        if form['bg'].file and form['bg'].filename:
+            fname = os.path.basename(form['bg'].filename).lower()
+            if fname.endswith(".png") or fname.endswith(".jpg") or fname.endswith(".gif"):
+                bg_file = form['bg'].file
+    
+    writePng(msg, bg_file)
 
 def cmd_main():
     msg = "unknown"
-    if len(sys.argv) > 2:
+    bg_file = None
+    if len(sys.argv) > 1:
         msg = sys.argv[1]
+    
+    if len(sys.argv) > 2:
+        bg_file = open(sys.argv[2], "rb")
+
+    writePng(msg, bg_file)
+
+def writePng(msg, bg_file=None):
     qrcode = segno.make(msg)
-    qrcode.save(sys.stdout.buffer, kind="png", scale=10)
+
+    if bg_file is None:
+        qrcode.save(sys.stdout.buffer, kind="png", scale=10)
+    else:
+        qrcode.to_artistic(background=bg_file, target=sys.stdout.buffer, scale=10, kind='png')
 
 def main():
     if 'REQUEST_METHOD' in os.environ:
